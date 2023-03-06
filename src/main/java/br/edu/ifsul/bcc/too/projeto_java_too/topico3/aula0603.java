2e447bac83b452aba1613fe064225ef830dbcb36
20221PF.CC0008 <20221PF.CC0008@pc-155968.passofundo.ifsul.local>
2023-03-06 11:45:39 -0300

    alteração de teste

---
+++ src/main/java/br/edu/ifsul/bcc/too/projeto_java_too/topico3/aula0603.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.bcc.too.projeto_java_too.topico3;
+
+/**
+ *
+ * @author 20221PF.CC0008
+ */
+public class aula0603 {
+    public static void main (string args[]){
+        double med;
+        int num1;
+        int num2;
+        
+    }
+    public static double media(int n1, int n2){
+        double med;
+        med = (n1 + n2)/2;
+        return (med);
+    }
+}
