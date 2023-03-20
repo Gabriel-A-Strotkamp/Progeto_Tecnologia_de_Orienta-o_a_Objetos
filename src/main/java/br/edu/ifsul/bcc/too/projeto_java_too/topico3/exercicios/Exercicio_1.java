d367b20bbd791dec96c8fb2b6e9b0f9380258d3f
20221PF.CC0008 <20221PF.CC0008@pc-155970.passofundo.ifsul.local>
2023-03-20 10:37:55 -0300

    salvar

---
+++ src/main/java/br/edu/ifsul/bcc/too/projeto_java_too/topico3/exercicios/Exercicio_1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.bcc.too.projeto_java_too.topico3.exercicios;
+
+/**
+ *
+ * @author 20221PF.CC0008
+ */
+
+import java.util.Scanner;
+
+public class Exercicio_1 {
+   public static void main(String[] args){
+       
+       int [][] rgDados1 = new int [3][3];
+       int [][] rgdados2 = new int [3][3];
+       int [][] rgdados3 = new int [3][3];
+       
+       Scanner sc = new Scanner (System.in);
+       
+       
+   }
+   
+}
